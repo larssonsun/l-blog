b776b1edfbcdb000e517db1e89a348e4bc747f4b
磊 孙 <77540975@qq.com>
2018-09-28 13:25:26 +0800

    Fix the code copy function bug in ie

--- static/js/blogdetail.js
+++ static/js/blogdetail.js
@@ -6,7 +6,7 @@
 $(document).ready(function(){
 
     //copy code to clipboard
-    $(".codehilite > pre").prepend("<span class='copy-code uk-text-right' uk-icon='icon: copy; ratio: 0.7'></span><br>")
+    $(".codehilite > pre").prepend("<a href='' class='copy-code uk-text-right' uk-icon='icon: copy; ratio: 0.7'></a><br>")
     $('.copy-code').on('click', function () {
         var code = $(this).parent().text();
         var ta = document.createElement('textarea');
@@ -14,16 +14,25 @@ $(document).ready(function(){
         ta.setAttribute('readonly', '');
         ta.style.position = 'absolute';
         ta.style.left = '-9999px';
-        document.body.appendChild(ta);
-        const selected = document.getSelection().rangeCount > 0 ?
+        
+        try{
+            //document.body.appendChild(ta);
+            this.appendChild(ta);
+            const selected = document.getSelection().rangeCount > 0 ?
             document.getSelection().getRangeAt(0) : false;
-        ta.select();
-        document.execCommand('copy');
-        document.body.removeChild(ta);
-        if (selected) {
-            document.getSelection().removeAllRanges();
-            document.getSelection().addRange(selected);
+            ta.select();
+            document.execCommand('copy');
+            // document.body.removeChild(ta);
+            this.removeChild(ta);
+            if (selected) {
+                document.getSelection().removeAllRanges();
+                document.getSelection().addRange(selected);
+            }
+            showMsg("code copied", "success")
+        }catch(e){
+            alert(e.message);
         }
+        
         return false;
     });
 });
