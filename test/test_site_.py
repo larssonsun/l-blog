2b856cba8e5098bc3c60bc46feef7f8d59065009
磊 孙 <77540975@qq.com>
2018-10-19 15:15:08 +0800

    Added pytest, maked site_.py's test

---
+++ test/test_site_.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+
+import pytest_aiohttp
+from aiohttp import web
+
+from site_ import Feeds_Atom, Feeds_Rss, Robots, Sitemap
+
+
+@pytest_aiohttp.pytest.fixture
+def resultPattern():
+    return dict(atomxml=r"[\s\S.\w]*</published></entry></feed>",
+                rssxml=r"[\s\S.\w]*</pubDate></item></channel></rss>",
+                sitemapxml=r"[\s\S.\w]*</priority></url></urlset>",
+                robotstxt=r"[\s\S.\w]*sitemap.xml")
+
+
+@pytest_aiohttp.pytest.mark.parametrize("viewPath, clientPath, resultPatternKey", [
+    (("/atom.xml", Feeds_Atom), "/atom.xml", "atomxml"),
+    (("/rss.xml", Feeds_Rss), "/rss.xml", "rssxml"),
+    (("/sitemap.xml", Sitemap), "/sitemap.xml", "sitemapxml"),
+    (("/robots.txt", Robots), "/robots.txt", "robotstxt")
+])
+async def test_Feeds_Atom(aiohttp_client, loop, viewPath, clientPath, resultPatternKey, resultPattern):
+    app = web.Application()
+    app.router.add_view(*viewPath, name="test")
+    client = await aiohttp_client(app)
+    resp = await client.get(clientPath)
+    assert resp.status == 200
+    text = await resp.text()
+    assert re.match(resultPattern[resultPatternKey], text)
